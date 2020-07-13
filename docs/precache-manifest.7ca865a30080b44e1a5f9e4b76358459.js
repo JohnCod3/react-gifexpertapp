91c725900a35e5279e4a4f269ae22a2452bf8c1c
John Fabio Giraldo <codigojohn@gmail.com>
2020-07-13 00:26:31 -0500

    Docs added.

---
+++ docs/precache-manifest.7ca865a30080b44e1a5f9e4b76358459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612fc8a30ce95f9fd9c3bbedd6d1477e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61f47e75384ed4e89cd1",
+    "url": "/static/css/main.97f1c51b.chunk.css"
+  },
+  {
+    "revision": "a50d5cb98697f7283138",
+    "url": "/static/js/2.0ba6f1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ba6f1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f47e75384ed4e89cd1",
+    "url": "/static/js/main.4cd430db.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
